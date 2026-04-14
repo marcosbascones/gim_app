80cd8872d7af366557ad7d94c09d0a4d275b8a0f
Marcos Bascones <marcosbascones1991@gmail.com>
2026-04-14 18:19:08 +0200

    primmer commit: backend springboot frontend vacio

---
+++ backend/src/test/java/com/reservas_gimnasio/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservas_gimnasio.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
