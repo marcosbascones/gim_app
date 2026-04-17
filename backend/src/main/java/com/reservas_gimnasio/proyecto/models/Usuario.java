263a9e355159d060257fecce3e8c3053a10ebeba
Marcos Bascones <marcosbascones1991@gmail.com>
2026-04-17 08:11:42 +0200

    comienzo modelo usuario

--- backend/src/main/java/com/reservas_gimnasio/proyecto/models/Usuario.java
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/models/Usuario.java
@@ -1,5 +1,43 @@
 package com.reservas_gimnasio.proyecto.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    public enum Rol {
+        USER, ADMIN
+    };
+
+    public enum Estado {
+        ACTIVO, BLOQUEADO
+    };
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
 
 }
