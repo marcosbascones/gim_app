d8b1237a70ba5b9b3aa04b2d4001328370733c14
Marcos Bascones <marcosbascones1991@gmail.com>
2026-04-17 19:22:47 +0200

    se añade UsuarioResponseDTO y crear y obtener usuario en service

--- backend/src/main/java/com/reservas_gimnasio/proyecto/Dto/usuario/UsuarioResponseDTO.java
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/Dto/usuario/UsuarioResponseDTO.java
@@ -1,5 +1,25 @@
 package com.reservas_gimnasio.proyecto.Dto.usuario;
 
+import com.reservas_gimnasio.proyecto.models.Usuario;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+//Lo que el usuario recibe por ejemplo se elimina password por seguridad 
 public class UsuarioResponseDTO {
+
+    private Long id;
+    private String email;
+    private String nombre;
+    private Usuario.Estado estado;
+    private Usuario.Rol rol;
+
 
 }
--- backend/src/main/java/com/reservas_gimnasio/proyecto/Services/UsuarioService.java
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/Services/UsuarioService.java
@@ -1,5 +1,59 @@
 package com.reservas_gimnasio.proyecto.Services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.reservas_gimnasio.proyecto.Dto.usuario.UsuarioRequestDTO;
+import com.reservas_gimnasio.proyecto.Dto.usuario.UsuarioResponseDTO;
+import com.reservas_gimnasio.proyecto.Repositories.UsuarioRepository;
+import com.reservas_gimnasio.proyecto.models.Usuario;
+
+@Service
+// Si todo va bien ok si no hace rollback
+@Transactional
+
 public class UsuarioService {
+
+    // Inyectamos la dependencia de repository y luego se la metemos al constructor
+    private final UsuarioRepository usuarioRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UsuarioService.class);
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public UsuarioResponseDTO crearUsuario(UsuarioRequestDTO request) {
+
+        if (usuarioRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email ya registrado");
+        }
+        Usuario usuario = new Usuario();
+        usuario.setNombre(request.getNombre());
+        usuario.setEmail(request.getEmail());
+        usuario.setPassword(request.getPassword());
+        usuario.setRol(request.getRol());
+        usuario.setEstado(Usuario.Estado.ACTIVO);
+
+        Usuario guardado = usuarioRepository.save(usuario);
+
+        logger.info("Usuario creado: {}", guardado);
+        return new UsuarioResponseDTO(guardado.getId(), guardado.getEmail(), guardado.getNombre(), guardado.getEstado(),
+                guardado.getRol());
+    }
+
+    public UsuarioResponseDTO obtenerUsuarioPorId(Long id) {
+
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe el usuario con id: " + id));
+
+        logger.info("Usuario encontrado: {}", usuario);
+        return new UsuarioResponseDTO(usuario.getId(), usuario.getEmail(), usuario.getNombre(), usuario.getEstado(),
+                usuario.getRol());
+
+    }
+
+    
 
 }
