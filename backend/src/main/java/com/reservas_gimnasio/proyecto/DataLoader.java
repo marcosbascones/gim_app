adcc9dcb3d5880dd488c8cec6a277dd747ad29df
Marcos Bascones <marcosbascones1991@gmail.com>
2026-04-18 16:13:19 +0200

    dataLoader con usuarios listo

--- backend/src/main/java/com/reservas_gimnasio/proyecto/DataLoader.java
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/DataLoader.java
@@ -1,9 +1,39 @@
 package com.reservas_gimnasio.proyecto;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.reservas_gimnasio.proyecto.Repositories.UsuarioRepository;
+import com.reservas_gimnasio.proyecto.models.Usuario;
 
 //Para inyectarlo en otras clases 
 @Component
-public class DataLoader {
+
+// CommandLineRunner es una interfaz de Spring Boot que permite ejecutar código
+// justo después de que la aplicación arranca.
+public class DataLoader implements CommandLineRunner {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public DataLoader(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        usuarioRepository.save(new Usuario(null, "Carlos García",
+                "carlos@gimnasio.com",
+                "1234",
+                Usuario.Rol.USER,
+                Usuario.Estado.ACTIVO));
+
+        usuarioRepository.save(new Usuario(null,
+                "Ana López",
+                "ana@gimnasio.com",
+                "1234",
+                Usuario.Rol.ADMIN,
+                Usuario.Estado.ACTIVO));
+
+    }
 
 }
