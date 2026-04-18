3383843eb3c55e8960b6538026ad269ca57f9ae2
Marcos Bascones <marcosbascones1991@gmail.com>
2026-04-18 15:49:48 +0200

    Finaliza UsuarioService y comienzo del DataLoader (datos de precargados)

---
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/DataLoader.java
@@ -0,0 +1,9 @@
+package com.reservas_gimnasio.proyecto;
+
+import org.springframework.stereotype.Component;
+
+//Para inyectarlo en otras clases 
+@Component
+public class DataLoader {
+
+}
--- backend/src/main/java/com/reservas_gimnasio/proyecto/Services/UsuarioService.java
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/Services/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.reservas_gimnasio.proyecto.Services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -54,6 +57,52 @@ public class UsuarioService {
 
     }
 
-    
+    public List<UsuarioResponseDTO> obtenerTodosLosUsuarios() {
+
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+        // Stream- Comienza el procesamiento, convierte una colleción en un flujo de
+        // datos que se puede procesar
+        // Map- Transforma cada elemento de la lista en lo que se indique en este
+        // ejemplo se dice que cada usuario va a transformarse en UsuarioResponseDTO
+        // collect(Collectors.toList())-Recoge el resultado y lo vuelve a transformar en
+        // una lista
+        logger.info("Obteniendo todos los usuarios");
+        return usuarios.stream()
+                .map(u -> new UsuarioResponseDTO(u.getId(), u.getEmail(), u.getNombre(), u.getEstado(), u.getRol()))
+                .collect(Collectors.toList());
+
+    }
+
+    // Con el id buscamos el usuario que queremos actualizarUsuario
+    // Con request traemos los nuevos datos que el usuaio ha introducido y que vamos
+    // a utilizar para actualizar la info
+
+    public UsuarioResponseDTO actualizarUsuario(Long id, UsuarioRequestDTO request) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe el usuario con id: " + id));
+
+        usuario.setNombre(request.getNombre());
+        usuario.setEmail(request.getEmail());
+        usuario.setPassword(request.getPassword());
+        usuario.setRol(request.getRol());
+
+        Usuario guardado = usuarioRepository.save(usuario);
+
+        logger.info("Usuario actualizado: {}", guardado);
+        return new UsuarioResponseDTO(guardado.getId(), guardado.getEmail(), guardado.getNombre(), guardado.getEstado(),
+                guardado.getRol());
+
+    }
+
+    public void eliminarUsuario(Long id) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe el usuario con id: " + id));
+
+        logger.info("Usuario con id: {} eliminado", id);
+
+        usuarioRepository.delete(usuario);
+
+    }
 
 }
