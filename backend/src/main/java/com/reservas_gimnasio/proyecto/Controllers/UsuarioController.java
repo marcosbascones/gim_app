c61fc773781ed96b8d1342b87891346de942c789
Marcos Bascones <marcosbascones1991@gmail.com>
2026-04-20 18:35:39 +0200

    UsuarioController completado

--- backend/src/main/java/com/reservas_gimnasio/proyecto/Controllers/UsuarioController.java
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/Controllers/UsuarioController.java
@@ -1,5 +1,62 @@
 package com.reservas_gimnasio.proyecto.Controllers;
 
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reservas_gimnasio.proyecto.Dto.usuario.UsuarioRequestDTO;
+import com.reservas_gimnasio.proyecto.Dto.usuario.UsuarioResponseDTO;
+import com.reservas_gimnasio.proyecto.Services.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+    
+
+    public UsuarioController(UsuarioService usuariosService) {
+        this.usuarioService = usuariosService;
+    }
+
+    @GetMapping
+    public List<UsuarioResponseDTO> obtenerTodosLosUsuarios() {
+        return usuarioService.obtenerTodosLosUsuarios();
+    }
+
+    @GetMapping("/{id}")
+
+    public UsuarioResponseDTO obtenerUsuarioPorId(@PathVariable Long id) {
+        return usuarioService.obtenerUsuarioPorId(id);
+    }
+
+    @PostMapping
+
+    public UsuarioResponseDTO crearUsuario(@RequestBody UsuarioRequestDTO request) {
+
+        return usuarioService.crearUsuario(request);
+    }
+
+    @PutMapping("/{id}")
+
+    public UsuarioResponseDTO actualizarUsuario(@PathVariable Long id, @RequestBody UsuarioRequestDTO request) {
+
+        return usuarioService.actualizarUsuario(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+
+    public void eliminarUsuario(@PathVariable Long id) {
+        usuarioService.eliminarUsuario(id);
+    }
 
 }
