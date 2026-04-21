1948c2c0e9162a5bf60a96e4aaf104026e3c8cab
Marcos Bascones <marcosbascones1991@gmail.com>
2026-04-21 08:02:39 +0200

    se crea archivo Test Unitario Service

---
+++ backend/src/test/java/com/reservas_gimnasio/proyecto/unit/UsuarioServiceTest.java
@@ -0,0 +1,22 @@
+package com.reservas_gimnasio.proyecto.unit;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.reservas_gimnasio.proyecto.Repositories.UsuarioRepository;
+import com.reservas_gimnasio.proyecto.Services.UsuarioService;
+
+@ExtendWith(MockitoExtension.class)
+public class UsuarioServiceTest {
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    
+
+}
