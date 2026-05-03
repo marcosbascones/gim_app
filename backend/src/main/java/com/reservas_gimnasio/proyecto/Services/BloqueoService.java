c27d144eb787fa1e97689c388fef06b13d36c5e9
Marcos Bascones <marcosbascones1991@gmail.com>
2026-05-03 13:43:04 +0200

    creado BloqueoService con metodo crearBloqueo

---
+++ backend/src/main/java/com/reservas_gimnasio/proyecto/Services/BloqueoService.java
@@ -0,0 +1,51 @@
+package com.reservas_gimnasio.proyecto.Services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.reservas_gimnasio.proyecto.Dto.Bloqueo.BloqueoRequestDTO;
+import com.reservas_gimnasio.proyecto.Dto.Bloqueo.BloqueoResponseDTO;
+import com.reservas_gimnasio.proyecto.Repositories.BloqueoRepository;
+import com.reservas_gimnasio.proyecto.Repositories.PistaRepository;
+import com.reservas_gimnasio.proyecto.models.Bloqueo;
+
+@Service
+public class BloqueoService {
+
+    private final BloqueoRepository bloqueoRepository;
+
+    private final PistaRepository pistaRepository;
+
+    private static final Logger logger= LoggerFactory.getLogger(BloqueoService.class);
+
+    public BloqueoService (BloqueoRepository bloqueoRepository, PistaRepository pistaRepository){
+        this.bloqueoRepository= bloqueoRepository;
+        this.pistaRepository= pistaRepository;
+    }
+
+    public BloqueoResponseDTO crearBloqueo( BloqueoRequestDTO requestDTO){
+
+        if(!bloqueoRepository.findByPistaAndFechaHoraInicioBeforeAndFechaHoraFinAfter(requestDTO.getPistaId(), requestDTO.getFechaHoraInicio(), requestDTO.getFechaHoraFin()).isEmpty()){
+            throw new RuntimeException("Bloqueo ya creado");
+        }
+
+        Bloqueo bloqueo = new Bloqueo();
+
+        bloqueo.setFechaHoraFin(requestDTO.getFechaHoraFin());
+
+        bloqueo.setFechaHoraInicio(requestDTO.getFechaHoraInicio());
+
+        bloqueo.setMotivo(requestDTO.getMotivo());
+
+        bloqueo.setPista(pistaRepository.findById(requestDTO.getPistaId())  .orElseThrow(() -> new RuntimeException("Pista no encontrada")));
+
+        Bloqueo guardado = bloqueoRepository.save(bloqueo);
+
+        logger.info("Bloqueo creada: {}", guardado);
+
+        return new BloqueoResponseDTO(guardado.getId(), guardado.getPista().getId(), guardado.getPista().getNombre(), guardado.getFechaHoraInicio(), guardado.getFechaHoraFin(), guardado.getMotivo());
+    }
+
+
+}
